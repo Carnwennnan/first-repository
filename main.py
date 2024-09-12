a65af02b0960dc9491a6d815ac9e7aa618d5c0d0
Carnwennnan <hcu8203@naver.com>
2024-09-12 13:21:08 +0900

    practice 2

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ if __name__ == '__main__':
     pn.is_prime_number(31)
     pn.is_prime_number(45)
     pn.is_prime_number(97)
+    print("Big Boys don't cry\nShoot low aim high\neat up stay thin\nStand out fit in")
+    print("Good girls don't fight\nBe you dress right\nWhite face thin skin\nStand out fit in")
